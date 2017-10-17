9492488b0ccdb32ef425810935dfb0ab62893d55
Natasha Scott <namscott@soniccat.com>
2017-10-17 14:19:16 -0400

    adjust tab selector for selected slide

--- js/tabs.js
+++ js/tabs.js
@@ -1,23 +1,23 @@
-const clearSelectedTabs = Array.from(document.getElementById('tab-toggles').getElementsByClassName('tab'));
 
 function initializeTabs() {
   const tab1 = document.querySelectorAll('[data-id="tab-1"]');
   const tab2 = document.querySelectorAll('[data-id="tab-2"]');
 
-  const setSelected1 = Array.from(tab1);
-  for (const item of setSelected1) {
+  const setSelected = Array.from(tab1);
+  for (const item of setSelected) {
     item.classList.add('selected');
   }
 
-  const unsetSelected1 = Array.from(tab2);
-  for (const item of unsetSelected1) {
+  const unsetSelected = Array.from(tab2);
+  for (const item of unsetSelected) {
     item.classList.remove('selected');
   }
 }
 
 function toggleTabs(e) {
   const selectedTab = e.target.getAttribute('data-id');
-  // const selectedTabId = selectedTab.split('tab-')[1];
+  const currentSlide = 'slide-' + window.location.hash.split('#/?slide=')[1];
+  const clearSelectedTabs = document.getElementById(currentSlide).getElementsByClassName('tab');
 
   for (const item of clearSelectedTabs) {
     item.classList.remove('selected');
@@ -27,7 +27,6 @@ function toggleTabs(e) {
   }
 }
 
-// const tabs = Array.from(document.getElementsByClassName('tab'));
 const toggle = Array.from(document.getElementsByClassName('toggle'));
 
 export default function load() {
