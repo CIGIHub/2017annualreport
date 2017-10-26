0981aeed2f32c19f5da1fe07388acddff80e392a
beccaneer <thomson.rebecca@gmail.com>
2017-10-26 11:03:58 -0400

    added hiding of slide content on caption mouseover

--- js/captions.js
+++ js/captions.js
@@ -1,15 +1,31 @@
-function findStandardSlide (element) {
-    while ((element = element.parentElement) && !element.className.contains("standard-slide"));
-    return element;
-}
-
-document.addEventListener("mouseover", function (eventArgs) {
-    var target = eventArgs.target;
-    while (target !== null) {
-        if (target.className.includes("photo-caption")) {
-            var standardSlide = findStandardSlide(target);
-            standardSlide.removeAttribute('background');
-            console.log(target);
+export default function photoCaptionMagic() {
+    
+    function findParentElement (element, className) {
+        while (element.className.indexOf(className) == -1){
+            element = element.parentElement;
         }
+        return element;  
+    } 
+    
+    function toggleText(element) {
+        var children = element.childNodes;
+        Array.from(children).forEach(function(child){
+            if ((child.className && child.className.indexOf("photo-caption") == -1 && child.className.indexOf("bottom-0") == -1) || (!child.className)){
+                child.hidden == true ? child.hidden = false : child.hidden = true;
+                if (child.childNodes) toggleText(child);
+            }
+        });
     }
-});
+    
+    var toggleBackground = function() {
+        var parentElement = findParentElement(this, "bottom-0");
+        toggleText(parentElement);
+    }
+
+    var photoCaptionElements = document.getElementsByClassName("photo-caption");
+    
+    Array.from(photoCaptionElements).forEach(function(element) {
+        element.addEventListener('mouseover', toggleBackground);
+        element.addEventListener('mouseout', toggleBackground);
+    });
+}
