257a12b2f67a022d268e9822eeaa3278b5727409
Ambyjkl <ambareeshbalaji@gmail.com>
2017-11-02 10:24:19 -0400

    Lots of mobile fixes

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,12 @@ import {
   createDiv,
   createEl,
   closeSvg,
-} from 'helpers';
+} from './helpers';
+
+import  {
+  putBackgroundImageIntoArticle,
+  unPutBackgroundImageIntoArticle,
+} from './mobile';
 
 import {
   changeSlideInUrl,
@@ -15,17 +20,21 @@ const globalShareFacebook = document.getElementById('global-share-facebook');
 const globalShareTwitter = document.getElementById('global-share-twitter');
 const tableOfContentsButton = document.getElementById('table-of-contents-button');
 const exploreCIGILink = document.getElementById('explore-cigi-link');
+const mobileHomeButton = document.getElementsByClassName('mobile-button')[0];
 const viewARLink = document.getElementById('view-ar-link');
 const cigiLogo = document.getElementById('cigi-logo');
 const mainTabs = document.getElementById('main-tabs');
 const header = document.getElementById('site-header');
 const upArrow = document.getElementsByClassName('explore')[0];
 const downArrow = document.getElementsByClassName('view-ar')[0];
+const chairsMessageLink = document.querySelector("[data-id='tab-2']");
 
 const smoothSlideContainer = document.getElementById('smooth-slide-container');
 const sections = document.getElementsByTagName('section');
 export const numberOfSections = sections.length;
 const slideNumToBackgroundVideo = new Array(numberOfSections);
+
+let mobile = false;
 
 function updateGlobalShareLinks() {
   const encodedURL = encodeURIComponent(location.href);
@@ -48,8 +57,7 @@ let sidebar;
 let topButton;
 let bottomButton;
 export let currentSlide = 1;
-let isScrolling = false;
-let tableOfContents;
+let isDesktopScrolling = false;
 let tocOpen = false;
 const buttons = new Array(numberOfSections);
 let fadeTimeoutSet = false;
@@ -59,14 +67,16 @@ const slideTransitionMs = 1000;
 
 export function disableOverlay() {
   overlayEnabled = false;
+  document.body.classList.remove('overflow-hidden');
 }
 
 export function enableOverlay() {
   overlayEnabled = true;
+  document.body.classList.add('overflow-hidden');
 }
 
-function updateNavigation(newIndex, oldIndex) {
-  if (isScrolling) {
+  function updateNavigation(newIndex, oldIndex, slide = true, transition = true) {
+  if (isDesktopScrolling) {
     return;
   }
   const newButton = buttons[newIndex];
@@ -82,15 +92,17 @@ function updateNavigation(newIndex, oldIndex) {
   if (newVideo) {
     newVideo.play();
   }
-  isScrolling = true;
-  setTimeout(() => {
-    isScrolling = false;
-  }, slideTransitionMs);
+  if (!mobile) {
+    isDesktopScrolling = true;
+    setTimeout(() => {
+      isDesktopScrolling = false;
+    }, slideTransitionMs);
+  }
   oldButton.classList.remove('active');
   newButton.classList.add('active');
   updateGlobalShareLinks();
   changeSlideInUrl(newIndex);
-  scrollViewToSlideIndex(newIndex);
+  if (slide) scrollViewToSlideIndex(newIndex, transition);
   currentSlide = newIndex;
   handleNavigationButtonsFade();
 }
@@ -113,6 +125,11 @@ function setMousemoveFadeTimemout(e) {
 exploreCIGILink.onclick = (e) => {
   e.stopPropagation();
   updateNavigation(0, currentSlide);
+};
+
+mobileHomeButton.onclick = (e) => {
+  e.stopPropagation();
+  updateNavigation(1, currentSlide);
 };
 
 upArrow.onclick = (e) => {
@@ -158,25 +175,33 @@ function handleNavigationButtonsFade() {
     viewARLink.classList.add('selected');
   }
 
-  if (currentSlide === 1) {
-    fadeOutAllNavigationComponents();
-    fadeOutNavigationComponent(mainTabs);
-    header.classList.add('white');
-  }
-  else {
-    fadeInNavigationComponent(mainTabs);
-    header.classList.remove('white');
-  }
-
   if (currentSlide === numberOfSections - 1) {
     fadeOutNavigationComponent(bottomButton);
   } else {
     fadeInNavigationComponent(bottomButton);
   }
+
+  if (currentSlide === 1) {
+    fadeOutAllNavigationComponents();
+    fadeOutNavigationComponent(mainTabs);
+    if (!mobile) {
+      header.classList.add('white');
+    }
+  }
+  else {
+    fadeInNavigationComponent(mainTabs);
+    if (!mobile) {
+      header.classList.remove('white');
+    }
+  }
 }
 
-function scrollViewToSlideIndex(newIndex) {
-  smoothSlideContainer.style.transition = `all ${slideTransitionMs}ms ease`;
+function scrollViewToSlideIndex(newIndex, transition = true) {
+  if (mobile) {
+    sections[newIndex].scrollIntoView();
+    return;
+  }
+  if (transition) smoothSlideContainer.style.transition = `all ${slideTransitionMs}ms ease`;
   requestAnimationFrame(() => {
     if (newIndex === 0) {
       smoothSlideContainer.style.transform = null;
@@ -191,26 +216,25 @@ function scrollViewToSlideIndex(newIndex) {
 
 function injectLinksAndAddSideBar() {
   sidebar = createDiv('fixed sidebar');
+  const slides = new Array(numberOfSections);
   const subsections = new Map();
   for (let i = 0; i < numberOfSections; i++) {
     // and add entry to table of contents
     const section = sections[i];
     const h3s = section.getElementsByTagName('h3');
-    const subsection = h3s.length && h3s[0].innerHTML || 'Explore CIGI' ;
+    const subsection = h3s.length && h3s[0].innerHTML || 'Explore CIGI';
     const slideName = section.getElementsByTagName('h1')[0].innerText;
+    slides[i] = slideName;
     slideNumToBackgroundVideo[i] = section.getElementsByClassName('fullscreen-bg-video')[0];
-
+    const item = {
+      slideName,
+      slideNum: i,
+    };
     const content = subsections.get(subsection);
     if (content === undefined) {
-        subsections.set(subsection, [{
-        title: slideName,
-        slidenum: i,
-      }]);
+      subsections.set(subsection, [item]);
     } else {
-      content.push({
-        title: slideName,
-        slidenum: i,
-      });
+      content.push(item);
     }
     // now create a corresponding button on the sidebar
     const button = createDiv(i === 1 ? 'sidebar-home fa-home' : 'sidebar-button');
@@ -226,61 +250,177 @@ function injectLinksAndAddSideBar() {
     sidebar.appendChild(button);
     buttons[i] = button;
   }
+
   buttons[currentSlide].classList.add('active');
-  tableOfContents = createDiv('pt5 pt6-ns fixed vh-100 w-100 bg-black-90 z-7 left-0 top-0');
+  const tableOfContentsDesktop = createDiv('pt5 pt6-ns pl3 pl0-ns pr3 pr0-ns fixed vh-100 w-100 bg-black-90 left-0 top-0');
+  const tableOfContentsMobile = createDiv('toc-mobile-wrapper');
+  const tableOfContentsWrapper = createDiv('z-7');
+  tableOfContentsWrapper.appendChild(tableOfContentsDesktop);
+
+  function mobileScroll() {
+    window.onscroll = null;
+    smoothSlideContainer.style.transform = null;
+    document.body.style.overflow = null;
+  }
+
+  function desktopScroll() {
+    window.onscroll = () => window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
+    document.body.style.overflow = 'hidden';
+  }
+  desktopScroll();
+
+  const handleWidth = () => {
+    if (window.innerWidth <= 450) {
+      if (!mobile) {
+        toggleTocEl();
+        mobileScroll();
+        scrollViewToSlideIndex(currentSlide);
+        if (tocOpen) {
+          header.style.background = 'black';
+        }
+        if (currentSlide === 1) {
+          header.classList.remove('white');
+        }
+        Array.from(sections).forEach(section => putBackgroundImageIntoArticle(section));
+      }
+    }
+    else {
+      if (mobile) {
+        toggleTocEl();
+        desktopScroll();
+        scrollViewToSlideIndex(currentSlide, false);
+        if (currentSlide === 1) {
+          header.classList.add('white');
+        }
+        unPutBackgroundImageIntoArticle();
+      }
+    }
+  };
+
+  const toggleTocEl = () => {
+    mobile = !mobile;
+    if (mobile) {
+      document.body.classList.add('mobile');
+      tableOfContentsDesktop.remove();
+      tableOfContentsWrapper.appendChild(tableOfContentsMobile);
+    } else {
+      document.body.classList.remove('mobile');
+      tableOfContentsMobile.remove();
+      tableOfContentsWrapper.appendChild(tableOfContentsDesktop);
+    }
+  };
+  window.onresize = handleWidth;
+  handleWidth();
   const tocIconOpen = 'fa fa-navicon black';
   const tocIcon = createEl('i', tocIconOpen);
   tableOfContentsButton.appendChild(tocIcon);
-  const toggleToc = () => {
+  const toggleTocOpen = () => {
     tocOpen = !tocOpen;
+    if (mobile) {
+      document.body.classList.add('overflow-hidden');
+    }
     if (tocOpen) {
       enableOverlay();
       fadeOutAllNavigationComponents();
       tocIcon.className = '';
       tocIcon.innerHTML = closeSvg;
-      document.body.appendChild(tableOfContents);
+      document.body.appendChild(tableOfContentsWrapper);
       cigiLogo.style.filter = 'invert(100%)';
       header.classList.add('white');
+      if (mobile) {
+        header.style.background = 'black';
+      }
     } else {
       tocIcon.innerHTML = '';
       tocIcon.className = tocIconOpen;
-      tableOfContents.remove();
+      tableOfContentsWrapper.remove();
       cigiLogo.style.filter = null;
       header.classList.remove('white');
+      if (mobile) {
+        header.style.background = null;
+      }
       disableOverlay();
     }
   };
+  tableOfContentsButton.onclick = toggleTocOpen;
 
-  tableOfContentsButton.onclick = toggleToc;
   document.body.appendChild(sidebar);
-  const container = createDiv('tr h-100 overflow-auto wrapper');
-  const h1 = createEl('h1', 'ttu accent-color fw5 f4 tracked lh-title mb2 mb4-ns');
-  h1.innerText = 'Table of Contents';
-  container.appendChild(h1);
-  const div = createDiv('col');
-  for (const [subsection, content] of subsections) {
-    const wrapper = createEl('li', 'column-no-wrap list');
-    const h2 = createEl('h2', 'accent-color partial-underline-right f4 fw3 mt3 smooth');
-    h2.innerText = subsection;
-    wrapper.appendChild(h2);
-    const ul = content.reduce((acc, item) => {
-      const li = createEl('li', 'dib pointer white f6 fw3 mt2 underline-hover list smooth');
-      li.innerHTML = item.title;
+
+  function tocDesktop() {
+    const container = createDiv('tr h-100 overflow-auto wrapper');
+    const h1 = createEl('h1', 'ttu accent-color fw5 f4 tracked lh-title mb2 mb4-ns db-ns dn');
+    h1.innerText = 'Table of Contents';
+    container.appendChild(h1);
+    const div = createDiv('col');
+    for (const [subsection, content] of subsections) {
+      const wrapper = createEl('li', 'column-no-wrap list');
+      const h2 = createEl('h2', 'accent-color partial-underline-right f4 fw3 mt3 smooth');
+      h2.innerText = subsection;
+      wrapper.appendChild(h2);
+      const ul = content.reduce((acc, slide) => {
+        const liClassName = 'dib pointer white f6 fw3 mt2 underline-hover list smooth';
+        const li = createEl('li', liClassName);
+        li.innerText = slide.slideName;
+        li.onclick = () => {
+          toggleTocOpen();
+          if (slide.slideNum !== currentSlide) {
+            updateNavigation(slide.slideNum, currentSlide);
+          }
+        };
+        acc.appendChild(li);
+        acc.appendChild(createEl('br'));
+        if (slide.slideNum === 3) {
+          const chairLi = createEl('li', liClassName);
+          chairLi.innerText = "Chair's Message";
+          chairLi.onclick = () => {
+            toggleTocOpen();
+            if (slide.slideNum !== currentSlide) {
+              updateNavigation(slide.slideNum, currentSlide);
+            }
+            chairsMessageLink.click();
+          };
+          acc.appendChild(chairLi);
+          acc.appendChild(createEl('br'));
+        }
+        return acc;
+      }, createEl('ul'));
+      wrapper.appendChild(ul);
+      div.appendChild(wrapper);
+    }
+    container.appendChild(div);
+    tableOfContentsDesktop.appendChild(container);
+  }
+  tocDesktop();
+
+  function tocMobile() {
+    const ul = createEl('ul');
+    slides.forEach((slideName, i) => {
+      const li = createEl('li', 'pointer');
+      li.innerText = slideName;
       li.onclick = () => {
-        toggleToc();
-        if (item.slidenum !== currentSlide) {
-          updateNavigation(item.slidenum, currentSlide);
+        toggleTocOpen();
+        if (i !== currentSlide) {
+          updateNavigation(i, currentSlide);
         }
       };
-      acc.appendChild(li);
-      acc.appendChild(createEl('br'));
-      return acc;
-    }, createEl('ul'));
-    wrapper.appendChild(ul);
-    div.appendChild(wrapper);
+      ul.appendChild(li);
+      if (i === 3) {
+        const chairLi = createEl('li', 'pointer');
+        chairLi.innerText = "Chair's Message";
+        chairLi.onclick = () => {
+          toggleTocOpen();
+          if (i !== currentSlide) {
+            updateNavigation(i, currentSlide);
+            chairsMessageLink.click();
+          }
+        };
+        ul.appendChild(chairLi);
+      }
+    });
+    tableOfContentsMobile.appendChild(ul);
   }
-  container.appendChild(div);
-  tableOfContents.appendChild(container);
+  tocMobile();
 }
 
 function injectTopAndBottomButtons() {
@@ -305,7 +445,7 @@ function injectTopAndBottomButtons() {
 }
 
 const navigationInactivityFadeHandler = e => {
-  if (overlayEnabled) {
+  if (overlayEnabled || mobile) {
     return;
   }
   clearTimeout(fadeTimeout);
@@ -315,7 +455,7 @@ const navigationInactivityFadeHandler = e => {
 };
 
 const navigationKeydownHandler = e => {
-  if (overlayEnabled) {
+  if (overlayEnabled || mobile) {
     return;
   }
   fadeInNavigationComponent(sidebar);
@@ -339,8 +479,23 @@ const navigationKeydownHandler = e => {
 };
 
 let lastWheel = 0;
+
+const mobileScrollHandler = () => {
+  if (!mobile) {
+    return;
+  }
+  const rect = sections[currentSlide].getBoundingClientRect();
+  const { top, bottom } = rect;
+  const windowHeight = window.innerHeight;
+  if (bottom < windowHeight / 3) {
+    updateNavigation(currentSlide + 1, currentSlide, false);
+  } else if (top > windowHeight / 3) {
+    updateNavigation(currentSlide - 1, currentSlide, false);
+  }
+};
+
 const navigationWheelHandler = e => {
-  if (overlayEnabled) {
+  if (overlayEnabled || mobile) {
     return;
   }
   const currentTime = performance.now();
@@ -360,16 +515,10 @@ const navigationWheelHandler = e => {
 };
 
 export function loadInitialSlide(initialSlide) {
-  updateNavigation(initialSlide, currentSlide);
+  updateNavigation(initialSlide, currentSlide, true, false);
 }
 
 export default function navigationMagic() {
-  // hacky onscroll to aid with the scroll-jacking
-  // scroll back to top when something like Find (Ctrl + F) triggers a scroll when not intended
-  window.onscroll = () => window.scrollTo(0, 0);
-  // hide the rest of the slides with overflow: hidden
-  document.body.style.overflow = 'hidden';
-
   updateGlobalShareLinks();
   injectLinksAndAddSideBar();
   injectTopAndBottomButtons();
@@ -377,4 +526,5 @@ export default function navigationMagic() {
   document.addEventListener('mousemove', navigationInactivityFadeHandler, false);
   document.addEventListener('keydown', navigationKeydownHandler, false);
   document.addEventListener('wheel', navigationWheelHandler, false);
+  window.addEventListener('scroll', mobileScrollHandler, false);
 }
