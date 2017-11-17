32505d71eea1925debdfb2b8b9244e802eb5f9b7
beccaneer <thomson.rebecca@gmail.com>
2017-11-17 09:14:39 -0500

    fixing img urls

--- js/tabs.js
+++ js/tabs.js
@@ -43,10 +43,10 @@ function setMessagesBackground(tab){
   let slide2 = document.getElementsByClassName('slide-2')[0];
   let slide2BG = slide2.getElementsByClassName('background-img')[0];
   if (tab == "tab-1"){
-    slide2BG.style.backgroundImage = "url('../assets/slides/RM-headshot-smaller.jpg')";
+    slide2BG.style.backgroundImage = "url('assets/slides/RM-headshot-smaller.jpg')";
   }
   else if (tab == "tab-2"){
-    slide2BG.style.backgroundImage = "url('../assets/slides/JB-headshot-smaller.jpg')";
+    slide2BG.style.backgroundImage = "url('assets/slides/JB-headshot-smaller.jpg')";
   } 
 }
 
