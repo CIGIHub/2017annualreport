b030ea3f9d810ffdf2fea1270138ae94a2c6701b
Natasha Scott <namscott@soniccat.com>
2017-12-05 16:01:53 -0500

    testing express.js

---
+++ assets/mail.js
@@ -0,0 +1,26 @@
+var nodemailer = require("nodemailer");
+var bodyParser = require('body-parser');
+var express = require('express');
+var app     = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.post('/mail', function (req, res) {
+
+var transporter = nodemailer.createTransport({
+    sendmail: true,
+    newline: 'unix',
+    path: '/usr/sbin/sendmail'
+});
+
+transporter.sendMail({
+    from: 'websiteupdates@cigionline.com',
+    to: req.query.email,
+    subject: 'CIGI 2017 Annual Report',
+    text: '<p>You have been invited to view our Annual report "<a href="https://cigionline.org/interactives/2017annualreport">.<p>'
+}, (err, info) => {
+    console.log(info.envelope);
+    console.log(info.messageId);
+});
+    
+
