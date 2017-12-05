085ea586571ffd8acb2e3ad4554348f5d45087f9
Natasha Scott <namscott@soniccat.com>
2017-12-05 16:03:36 -0500

    fix parse error

--- assets/mail.js
+++ assets/mail.js
@@ -7,20 +7,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/mail', function (req, res) {
 
-var transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix',
-    path: '/usr/sbin/sendmail'
-});
+    var transporter = nodemailer.createTransport({
+        sendmail: true,
+        newline: 'unix',
+        path: '/usr/sbin/sendmail'
+    });
 
-transporter.sendMail({
-    from: 'websiteupdates@cigionline.com',
-    to: req.query.email,
-    subject: 'CIGI 2017 Annual Report',
-    text: '<p>You have been invited to view our Annual report "<a href="https://cigionline.org/interactives/2017annualreport">.<p>'
-}, (err, info) => {
-    console.log(info.envelope);
-    console.log(info.messageId);
+    transporter.sendMail({
+        from: 'websiteupdates@cigionline.com',
+        to: req.query.email,
+        subject: 'CIGI 2017 Annual Report',
+        text: '<p>You have been invited to view our Annual report "<a href="https://cigionline.org/interactives/2017annualreport">.<p>'
+    }, (err, info) => {
+        console.log(info.envelope);
+        console.log(info.messageId);
+    });
 });
     
 
