c9814a3a62849ac0537165f8f5cf42f60f52411b
Natasha Scott <namscott@soniccat.com>
2017-12-05 16:46:00 -0500

    more email tests

--- assets/mail.js
+++ assets/mail.js
@@ -1,32 +1,46 @@
-var nodemailer = require("nodemailer");
-var bodyParser = require('body-parser');
-var express = require('express');
-var app     = express();
+var nodemailer = require('nodemailer');
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+// Create a SMTP transport object
+var transport = nodemailer.createTransport("SMTP", {
+    service: 'Hotmail',
+    auth: {
+        user: "username",
+        pass: "password"
+    }
+  });
 
-app.post('/mail', function (req, res) {
+console.log('SMTP Configured');
 
-    var transporter = nodemailer.createTransport({
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail'
-    });
+// Message object
+ var message = {
 
-    var mailOptions = {
-        from: 'websiteupdates@cigionline.org',
-        to: 'namscott@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-    };
+// sender info
+from: from,
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+// Comma separated list of recipients
+to: req.query.to ,
 
+// Subject of the message
+subject:req.query.subject //'Nodemailer is unicode friendly ✔', 
+
+// plaintext body
+text: req.query.text //'Hello to myself!',
+
+// HTML body
+ /*  html:'<p><b>Hello</b> to myself <img src="cid:note@node"/></p>'+
+     '<p>Here\'s a nyan cat for you as an embedded attachment:<br/></p>'*/
+};
+
+console.log('Sending Mail');
+transport.sendMail(message, function(error){
+if(error){
+  console.log('Error occured');
+  console.log(error.message);
+  return;
+}
+ console.log('Message sent successfully!');
+
+ // if you don't want to use this transport object anymore,uncomment    
+ //transport.close(); // close the connection pool
 });
    
